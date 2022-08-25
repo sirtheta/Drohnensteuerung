3adbf2bce579deadca09f863ce7d66a72260b23c
sirtheta <neo38@bluewin.ch>
2022-08-25 09:50:24 +0200

    adds testapp

---
+++ ComTestArduino/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WinFormsApp1
+{
+  partial class Form1
+  {
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.send = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.btnConnect = new System.Windows.Forms.Button();
+      this.btnDisconnect = new System.Windows.Forms.Button();
+      this.label2 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // send
+      // 
+      this.send.Location = new System.Drawing.Point(86, 122);
+      this.send.Name = "send";
+      this.send.Size = new System.Drawing.Size(75, 23);
+      this.send.TabIndex = 0;
+      this.send.Text = "Send";
+      this.send.UseVisualStyleBackColor = true;
+      this.send.Click += new System.EventHandler(this.send_Click);
+      // 
+      // textBox1
+      // 
+      this.textBox1.Font = new System.Drawing.Font("Segoe UI Variable Text", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.textBox1.Location = new System.Drawing.Point(86, 68);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(227, 34);
+      this.textBox1.TabIndex = 1;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Segoe UI Variable Text", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.label1.Location = new System.Drawing.Point(319, 68);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(62, 27);
+      this.label1.TabIndex = 2;
+      this.label1.Text = "label1";
+      // 
+      // btnConnect
+      // 
+      this.btnConnect.Location = new System.Drawing.Point(86, 12);
+      this.btnConnect.Name = "btnConnect";
+      this.btnConnect.Size = new System.Drawing.Size(75, 23);
+      this.btnConnect.TabIndex = 3;
+      this.btnConnect.Text = "Connect";
+      this.btnConnect.UseVisualStyleBackColor = true;
+      this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+      // 
+      // btnDisconnect
+      // 
+      this.btnDisconnect.Location = new System.Drawing.Point(202, 12);
+      this.btnDisconnect.Name = "btnDisconnect";
+      this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+      this.btnDisconnect.TabIndex = 4;
+      this.btnDisconnect.Text = "Disconect";
+      this.btnDisconnect.UseVisualStyleBackColor = true;
+      this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Segoe UI Variable Text", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.label2.Location = new System.Drawing.Point(316, 12);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(65, 27);
+      this.label2.TabIndex = 5;
+      this.label2.Text = "label2";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.btnDisconnect);
+      this.Controls.Add(this.btnConnect);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.send);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private Button send;
+        private TextBox textBox1;
+        private Label label1;
+        private Button connect;
+    private Button btnConnect;
+    private Button btnDisconnect;
+    private Label label2;
+  }
+}
